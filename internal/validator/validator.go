3125987d3628819b5e196fd7a35d04f5af9d5984
Arsen Hovhannisyan <arsen1994t@gmail.com>
2022-10-30 22:13:33 +0400

    Minor changes to use generics.

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -49,7 +49,7 @@ func MinChars(value string, n int) bool {
 	return utf8.RuneCountInString(value) >= n
 }
 
-func PermittedInt(value int, permittedValues ...int) bool {
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	for i := range permittedValues {
 		if value == permittedValues[i] {
 			return true
